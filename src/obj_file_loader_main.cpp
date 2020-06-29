9ef8ce8047f1181c55eec286d6f2de29c5346fcf
Akira Kawata <akirakawata1@gmail.com>
2020-06-29 22:55:11 +0900

    Rename to obj_file_loader_main.cpp

---
+++ src/obj_file_loader_main.cpp
@@ -0,0 +1,7 @@
+﻿#include "obj_file_loader.h"
+
+int main() {
+    double lx = 0, ly = 0, rx = 0, ry = 0;
+    obj_file_loader::run_main("LibertStatue/LibertStatue.obj", &lx, &rx, &ly,
+                              &ry);
+}
--- src/render2.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "obj_file_loader.h"
-
-int main() {
-    double lx = 0, ly = 0, rx = 0, ry = 0;
-    obj_file_loader::run_main("LibertStatue.obj", &lx, &rx, &ly, &ry);
-}
