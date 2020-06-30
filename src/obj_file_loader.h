cf99fd03e6442232730cf462c75a47664fd05964
Akira Kawata <akirakawata1@gmail.com>
2020-06-30 21:30:47 +0900

    Use glog

--- src/obj_file_loader.h
+++ src/obj_file_loader.h
@@ -1,7 +1,10 @@
 #include <string>
 
+#include <glog/logging.h>
+
 namespace obj_file_loader {
 int run_main(std::string objfile_path, const double *const left_eye_center_x,
-             const double *const right_eye_center_x, const double *const left_eye_center_y,
+             const double *const right_eye_center_x,
+             const double *const left_eye_center_y,
              const double *const right_eye_center_y);
 }
--- src/obj_file_loader_main.cpp
+++ src/obj_file_loader_main.cpp
@@ -1,6 +1,9 @@
 ﻿#include "obj_file_loader.h"
 
-int main() {
+int main(int argc, char **argv) {
+    google::InitGoogleLogging(argv[0]);
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
     double lx = 0, ly = 0, rx = 0, ry = 0;
     obj_file_loader::run_main("LibertStatue/LibertStatue.obj", &lx, &rx, &ly,
                               &ry);
