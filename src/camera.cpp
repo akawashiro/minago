465ca7388a9f994602a96e3779ece1def884643e
Akira Kawata <akirakawata1@gmail.com>
2020-06-30 21:31:10 +0900

    Dump frame data for debug

--- src/camera.cpp
+++ src/camera.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/objdetect/objdetect.hpp>
 
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 
 /* Attempt at supporting openCV version 4.0.1 or higher */
@@ -17,11 +18,110 @@
 #endif
 
 namespace camera {
+
+size_t length_of_serialize_data(camera::rs2_frame_data frame) {
+    // the first 4 bytes of serialized data is the length.
+    return sizeof(uint32_t) * 4 +
+           frame.height * frame.width * 3 * sizeof(uint8_t) +
+           frame.n_points * sizeof(rs2::vertex) +
+           frame.n_points * sizeof(rs2::texture_coordinate);
+}
+
+void save_frame(rs2_frame_data frame, const std::string &path) {
+    size_t len = length_of_serialize_data(frame);
+    char *buf = (char *)malloc(len);
+    char *p = buf;
+
+    *((uint32_t *)p) = (uint32_t)len;
+    p += sizeof(uint32_t);
+
+    *((uint32_t *)p) = frame.height;
+    p += sizeof(uint32_t);
+
+    *((uint32_t *)p) = frame.width;
+    p += sizeof(uint32_t);
+
+    *((uint32_t *)p) = frame.n_points;
+    p += sizeof(uint32_t);
+
+    memcpy(p, frame.rgb.get(),
+           frame.height * frame.width * 3 * sizeof(uint8_t));
+    p += frame.height * frame.width * 3 * sizeof(uint8_t);
+
+    memcpy(p, frame.vertices.get(), frame.n_points * sizeof(rs2::vertex));
+    p += frame.n_points * sizeof(rs2::vertex);
+
+    memcpy(p, frame.texture_coordinates.get(),
+           frame.n_points * sizeof(rs2::texture_coordinate));
+
+    std::ofstream f(path, std::ios::out | std::ios::binary);
+    f.write(buf, len);
+    f.close();
+    free(buf);
+}
+
+rs2_frame_data read_frame(const std::string &path) {
+    char lenbuf[4];
+    char *buf = (char *)malloc(FRAME_DUMP_MAX);
+
+    std::ifstream f(path, std::ios::in | std::ios::binary);
+    f.read(lenbuf, 4);
+
+    size_t len = *((uint32_t *)lenbuf);
+    f.seekg(0, std::ios::beg);
+    f.read(buf, len);
+
+    rs2_frame_data frame;
+    char *p = buf;
+
+    // Skip the first 4 bytes of the length of the serialized data.
+    p += sizeof(uint32_t);
+
+    frame.height = *((uint32_t *)p);
+    p += sizeof(uint32_t);
+
+    frame.width = *((uint32_t *)p);
+    p += sizeof(uint32_t);
+
+    frame.n_points = *((uint32_t *)p);
+    p += sizeof(uint32_t);
+
+    LOG(INFO) << "frame.height = " << frame.height
+              << ", frame.width = " << frame.width
+              << ", frame.n_points = " << frame.n_points;
+
+    std::shared_ptr<uint8_t> rgb_tmp(
+        new uint8_t[3 * frame.width * frame.height],
+        std::default_delete<uint8_t[]>());
+    frame.rgb = rgb_tmp;
+    memcpy(frame.rgb.get(), p,
+           sizeof(uint8_t) * 3 * frame.width * frame.height);
+    p += sizeof(uint8_t) * 3 * frame.width * frame.height;
+
+    std::shared_ptr<rs2::vertex> vertices_tmp(
+        new rs2::vertex[frame.n_points], std::default_delete<rs2::vertex[]>());
+    frame.vertices = vertices_tmp;
+    memcpy(frame.vertices.get(), p, sizeof(rs2::vertex) * frame.n_points);
+    p += sizeof(rs2::vertex) * frame.n_points;
+
+    std::shared_ptr<rs2::texture_coordinate> texture_coordinates_tmp(
+        new rs2::texture_coordinate[frame.n_points],
+        std::default_delete<rs2::texture_coordinate[]>());
+    frame.texture_coordinates = texture_coordinates_tmp;
+    memcpy(frame.texture_coordinates.get(), p,
+           sizeof(rs2::texture_coordinate) * frame.n_points);
+
+    return frame;
+}
+
 int camera_main_loop(
     ThreadSafeState<eye_like::EyesPosition>::ThreadSafeStatePutViewer
         &eye_pos_put,
     ThreadSafeQueue<rs2_frame_data>::ThreadSafeQueuePushViewer &frame_queue,
-    bool use_realsense) try {
+    bool use_realsense, bool debug = false) try {
+
+    LOG(INFO) << "camera_main_loop start";
+
     eye_like::init();
     if (use_realsense) {
         // Declare RealSense pipeline, encapsulating the actual device and
@@ -96,6 +196,11 @@ int camera_main_loop(
                 memcpy(f.texture_coordinates.get(),
                        points.get_texture_coordinates(),
                        sizeof(rs2::texture_coordinate) * f.n_points);
+
+                if (debug) {
+                    save_frame(f, realsense_frame_dump_file);
+                    read_frame(realsense_frame_dump_file);
+                }
 
                 frame_queue.push(f);
             }
--- src/camera.h
+++ src/camera.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <atomic>
+#include <string>
+
+#include <glog/logging.h>
 #include <librealsense2/rs.hpp>
 
 #include "eyeLike.h"
@@ -11,6 +14,11 @@ namespace camera {
 const int FPS = 15;
 const int FRAME_WIDTH = 640;
 const int FRAME_HEIGHT = 360;
+const int FRAME_DUMP_MAX =
+    sizeof(uint32_t) * 4 + FRAME_HEIGHT * FRAME_WIDTH * 3 * sizeof(uint8_t) +
+    FRAME_HEIGHT * FRAME_WIDTH * sizeof(rs2::vertex) +
+    FRAME_HEIGHT * FRAME_WIDTH * sizeof(rs2::texture_coordinate);
+const std::string realsense_frame_dump_file = "../misc/realsense_frame_dump";
 
 struct rs2_frame_data {
     uint32_t height, width, n_points;
@@ -19,8 +27,12 @@ struct rs2_frame_data {
     std::shared_ptr<rs2::texture_coordinate> texture_coordinates;
 };
 
+void save_frame(rs2_frame_data frame, const std::string &path);
+
+rs2_frame_data read_frame(const std::string &path);
+
 int camera_main_loop(
     ThreadSafeState<eye_like::EyesPosition>::ThreadSafeStatePutViewer &eye_pos,
     ThreadSafeQueue<rs2_frame_data>::ThreadSafeQueuePushViewer &frame_queue,
-    bool use_realsense);
+    bool use_realsense, bool debug);
 } // namespace camera
